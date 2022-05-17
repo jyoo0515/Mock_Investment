1e66c3a967d925b233d48d928abae26952295bb2
jyoo0515 <jasonyoo0515@gmail.com>
2022-05-17 15:58:59 +0900

    fix: removed error causing file

--- client/src/recoil/auth/useUser.ts
+++
@@ -1,9 +0,0 @@
-import { useRecoilValue } from 'recoil';
-import authState from './atom';
-
-function useUser() {
-  const auth = useRecoilValue(authState);
-  return auth.user;
-}
-
-export default useUser;
